8dccb040bffb1fe34c88917468360e41e8905110
pauline muthoni <kinyanjuipauline69@gmail.com>
2025-04-27 17:20:37 +0300

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import PlantList from "./PlantList";
+import NewPlantForm from "./NewPlantForm";
+import Search from "./Search";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
 function App() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const BASE_URL = "http://localhost:6001/plants";
+
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then((res) => res.json())
+      .then(setPlants);
+  }, []);
+
+  function addPlant(newPlant) {
+    setPlants([...plants, newPlant]);
+  }
+
+  function toggleSoldOut(id) {
+    setPlants(
+      plants.map((plant) =>
+        plant.id === id ? { ...plant, isSoldOut: !plant.isSoldOut } : plant
+      )
+    );
+  }
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
   return (
     <div className="app">
       <Header />
       <PlantPage />
+      <Search searchTerm={searchTerm} onSearchChange={setSearchTerm} />
+      <PlantList plants={filteredPlants} onToggleSoldOut={toggleSoldOut} />
     </div>
   );
 }
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
-function PlantPage() {
+function PlantPage({
+  plants,
+  onAddPlant,
+  onUpdatePlant,
+  searchTerm,
+  onSearchChange,
+}) {
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={onAddPlant} />
+      <Search searchTerm={searchTerm} onSearchChange={onSearchChange} />
+      <PlantList plants={plants} onUpdatePlant={onUpdatePlant} />
     </main>
   );
 }
